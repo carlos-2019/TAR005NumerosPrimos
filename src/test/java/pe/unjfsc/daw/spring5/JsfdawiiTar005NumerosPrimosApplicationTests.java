111e428a78316d05b465761a187d6b8ba061611b
borda-2019 <bordanonatocarlos@gmail.com>
2020-08-16 10:46:21 -0500

    Iniciando El proyecto

---
+++ src/test/java/pe/unjfsc/daw/spring5/JsfdawiiTar005NumerosPrimosApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.unjfsc.daw.spring5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsfdawiiTar005NumerosPrimosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
